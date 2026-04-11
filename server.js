78380e5f70b0890758bb5f66ee457711f863b2fa
Pernocte <114966775+Pernocte@users.noreply.github.com>
2026-04-11 08:45:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -260,7 +260,7 @@ app.post('/api/perfil/foto', async (req, res) => {
 // ==========================================
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
